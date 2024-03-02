343534f136b0bc3717d1aee638a2b1a498f07c4e
Juan Jose Duque Cardona <47718782+jjduquec@users.noreply.github.com>
2024-03-02 10:33:32 -0500

    first commit jduque

---
+++ Taller4/src/main/java/jduque.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class jduque {
+    
+    
+    public jduque (){
+
+        
+        System.out.println("Esta es una clase de Juan Duque");
+        
+    }
+    
+}
